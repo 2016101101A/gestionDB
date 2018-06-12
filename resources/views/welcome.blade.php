32f4c6ed4274b9c73d0c68f23084d5cbacf1d45f
Ruben Areste Pirca <ruben.art28@gmail.com>
2018-06-12 15:19:17 -0500

    tarea 08-06-2018

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,87 +9,93 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="C:\Users\ad\gestionDB\public\css\estilos.css" rel="stylesheet" type="text/css">
+        <script src="/js/majax.js"></script>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <button id="btn" style="background:#FE2E9A; height:34px; margin:0 auto; margin-right:10px; float:left;text-align:center">
+            OBTENER MATERIALES
+        </button>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+        <script>
+            Majax.setConfig(2,'6e2eIb6UuteHJMWmRKdUlvQbmE3WpWYUh86OFHck','');
+            function obtenerMateriales(e){
+                    e.preventDefault();
+                    var majax = new Majax();
+                    majax.get(
+                        '/api/materials',
+                        {
+                            valid: function(r){
+                                console.info(r);
+                                for(i=0;i<r.data.length;i++){
+                                    document.getElementById('s1').innerHTML=document.getElementById('s1').innerHTML+r.data[i].titulo+r.data[i].idioma+r.data[i].tipo+"<br>";
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+                                }
+                            },
+                            error:function(error){
+                                console.error(error);
+                            }
+                        }
+                    );
+            }
+            document.getElementById('btn').addEventListener('click',obtenerMateriales);
+            </script>
+            <button id="btn1"style="background:#FE9A2E; height:34px; margin:0 auto; margin-right:10px; float:left;text-align:center">
+            OBTENER IDIOMAS</button>
+
+            <script>
+        Majax.setConfig(2,'6e2eIb6UuteHJMWmRKdUlvQbmE3WpWYUh86OFHck','');
+        function obtenerIdiomas(e){
+            e.preventDefault();
+            var majax= new Majax();
+            majax.get(
+                '/api/languages',
+                {
+                    valid: function(r){
+                        console.info(r);
+                        for(i=0;i<r.data.length;i++){
+                                    document.getElementById('s1').innerHTML=document.getElementById('s1').innerHTML+r.data[i].idioma+"<br>";
+                                }
+                    },
+                    error: function(error){
+                        console.error(error);
+                    }
+                }
+            );
+        }
+        document.getElementById('btn1').addEventListener('click',obtenerIdiomas);
+        </script>
+        <button id="btn2"style="background:#819FF7; height:34px; margin-left: auto;
+  margin-right: auto; margin-right:10px; float:left; text-align:center">
+            OBTENER TIPOS DE MATERIAL
+        </button>
+
+        <script>
+        Majax.setConfig(2,'6e2eIb6UuteHJMWmRKdUlvQbmE3WpWYUh86OFHck','');
+        function obtenerTipodeMateriales(e){
+            e.preventDefault();
+            var majax= new Majax();
+            majax.get(
+                '/api/materialstype',
+                {
+                    valid: function(r){
+                        console.info(r);
+                        for(i=0;i<r.data.length;i++){
+                                    document.getElementById('s1').innerHTML=document.getElementById('s1').innerHTML+r.data[i].tipo+"<br>";
+
+                                }
+                    },
+                    error: function(error){
+                        console.error(error);
+                    }
+                }
+            );
+        }
+        document.getElementById('btn2').addEventListener('click',obtenerTipodeMateriales);
+        </script>
+        
+        <div id="s1"></div>
     </body>
 </html>
